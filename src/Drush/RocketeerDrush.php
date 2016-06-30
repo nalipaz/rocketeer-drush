814f707c5ee6c67ad1ad7c505b1fb27cb2200609
Nicholas Alipaz <nalipaz@labiomed.org>
2016-06-30 11:55:08 -0700

    trying to add sql dump

--- src/Drush/RocketeerDrush.php
+++ src/Drush/RocketeerDrush.php
@@ -4,6 +4,7 @@ namespace Rocketeer\Plugins\Drush;
 use Illuminate\Container\Container;
 use Rocketeer\Abstracts\AbstractPlugin;
 use Rocketeer\Services\TasksHandler;
+use Rocketeer\Plugins\Drush\Tasks\DrushSqlDump;
 
 class RocketeerDrush extends AbstractPlugin {
 
@@ -40,18 +41,22 @@ class RocketeerDrush extends AbstractPlugin {
    * @return void
    */
   public function onQueue(TasksHandler $queue) {
-    // Would've preferred to use $queue->addTaskListeners('deploy', 'before-symlink', function($task), but it runs three times...
-    $queue->before('deploy', function ($task) {
-      $drush = $task->binary('Rocketeer\Plugins\Drush\Binaries\Drush');
-      $drush->setSiteAlias($this->getConfig($task, 'drush_alias'));
-      $drush->run('siteSet');
-      $drush->run('sqlDump', $task->releasesManager->getCurrentRelease() . '.sql');
-    });
+//    $drushSqlDump = new DrushSqlDump($this->app);
+//    $drushSqlDump->setDrush($this);
+//    $queue->addTaskListeners('deploy', 'before', [clone $drushSqlDump], -10, true);
+//
+//    // Would've preferred to use $queue->addTaskListeners('deploy', 'before-symlink', function($task), but it runs three times...
+//    $queue->before('deploy', function ($task) {
+//      $drush = $task->binary('Rocketeer\Plugins\Drush\Binaries\Drush');
+//      $drush->setSiteAlias($this->getConfig($task, 'drush_alias'));
+//      $drush->run('siteSet');
+//      $drush->run('sqlDump', $task->releasesManager->getCurrentRelease() . '.sql');
+//    });
     $queue->after('deploy', function ($task) {
       $drush = $task->binary('Rocketeer\Plugins\Drush\Binaries\Drush');
       $drush->setSiteAlias($this->getConfig($task, 'drush_alias'));
-      $drush->run('sqlDump', $task->releasesManager->getCurrentRelease() . '.sql');
       $drush->run('siteSet');
+      $drush->run('sqlDump', $task->releasesManager->getCurrentRelease() . '.sql');
       $drush->run('configImport', $this->getConfig($task, 'drupal_config'));
       $drush->run('updatedb');
       $drush->run('advaggClearAllFiles');
