06e0b0f3e83c6b55e0abfc425d1bdb0786b637c3
Nicholas Alipaz <nalipaz@labiomed.org>
2016-09-09 10:58:59 -0700

    Attempt to prompt for drush alias on deploy

--- src/Drush/DrushBaseTask.php
+++ src/Drush/DrushBaseTask.php
@@ -16,7 +16,6 @@ class DrushBaseTask extends \Rocketeer\Abstracts\AbstractTask {
 
     $this->drush = $this->binary($drushPlugin->binaryPath);
     $this->drushPlugin = $drushPlugin;
-    $this->drush->setSiteAlias($this->drushPlugin->getConfig($this, 'drush_alias'));
   }
 
   /**
@@ -24,5 +23,12 @@ class DrushBaseTask extends \Rocketeer\Abstracts\AbstractTask {
    *
    * @return void
    */
-  public function execute() {}
+  public function execute() {
+    $drush_alias = $this->localStorage->get('drush_alias');
+    if (!$drush_alias) {
+      $drush_alias = $this->command->ask('What Drush Alias should be used for this deployment?');
+      $this->localStorage->set('drush_alias', $this->drush->ensureAliasFormat($drush_alias));
+    }
+    $this->drush->setSiteAlias($this->localStorage->get('drush_alias'));
+  }
 }
